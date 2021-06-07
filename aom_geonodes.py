c12a412e60f9340e2ecea36aa4b6ef52a43a2fd2
Modicolitor <Bjoern.Eckhardt@gmx.de>
2021-06-07 17:39:39 +0200

    spray node links problems, improve node to python

--- aom_geonodes.py
+++ aom_geonodes.py
@@ -43,7 +43,7 @@ class AOMGeoNodesHandler:
 
                 for cob in cole.objects:
                     if ob.name == cob.name:
-                        #print("unline ob {ob.name} col {col} ")
+                        # print("unline ob {ob.name} col {col} ")
                         cole.objects.unlink(ob)
 
     def add_collection_to_modinp(self, col, mod, modinput):
@@ -58,7 +58,7 @@ class AOMGeoNodesHandler:
         mod, nodegroup = self.new_geonodes_mod(ocean)
         mod.name = "Spray"
         nodegroup.name = "Spray"
-        #self.move_mod_one_up(ocean, mod)
+        # self.move_mod_one_up(ocean, mod)
         self.make_spray_nodes(mod, mod.node_group)
 
         # collection
@@ -76,29 +76,31 @@ class AOMGeoNodesHandler:
 
         self.ob_to_collection(context, collection, context.object)
 
-        #context.layer_collection.children['Spray'].exclude = True
-
         # it will be only one but more flexible
         for inp in mod.node_group.inputs:
             if inp.bl_socket_idname == 'NodeSocketCollection':
                 self.add_collection_to_modinp(collection, mod, inp)
 
-        #context.view_layer.active_layer_collection = collection
-        #collection.exclude = True
-        #self.AdvCollection.children[collection.name].exclude = True
+        context.layer_collection.children['Spray'].exclude = True
+        dg = bpy.context.evaluated_depsgraph_get()
+
+        context.view_layer.objects.active = ocean
+        # collection.exclude = True
+        # self.AdvCollection.children[collection.name].exclude = True
 
     def make_spray_nodes(self, mod, node_group):
         self.remove_nodes(node_group)
         nodes = node_group.nodes
         links = node_group.links
 
+        inp = node_group.inputs.new('NodeSocketFloat', 'Height')
+        inp.default_value = 1.0
         inp = node_group.inputs.new('NodeSocketFloat', 'Density Max')
-        inp = node_group.inputs.new('NodeSocketFloat', 'Density Max')
-        inp.default_value = 30.0
+        inp.default_value = 50.0
         inp = node_group.inputs.new('NodeSocketFloat', 'Contrast')
-        inp.default_value = 0.25
+        inp.default_value = 1.2
         inp = node_group.inputs.new('NodeSocketFloat', 'MaxParticleScale')
-        inp.default_value = 0.8
+        inp.default_value = 0.5
         inp = node_group.inputs.new('NodeSocketFloat', 'MinParticleScale')
         inp.default_value = 0.01
         inp = node_group.inputs.new('NodeSocketFloat', 'OverallParticleScale')
@@ -110,87 +112,65 @@ class AOMGeoNodesHandler:
 
         node = nodes.new("NodeGroupInput")
         node.name = "Group Input"
-        node.location = (-1160, 129)
+        node.location = (-1321, 150)
 
         node = nodes.new("ShaderNodeMath")
         node.name = "Math"
         node.location = (-984, 374)
+        node.operation = "ADD"
+        node.use_clamp = False
         node.inputs[0].default_value = 0.5
-        node.operation = "ADD"
         node.inputs[1].default_value = 1.0
-        node.operation = "ADD"
         node.inputs[2].default_value = 0.0
-        node.operation = "ADD"
         node.outputs[0].default_value = 0.0
 
         node = nodes.new("ShaderNodeMath")
         node.name = "Math.001"
         node.location = (-782, 368)
+        node.operation = "LOGARITHM"
+        node.use_clamp = False
         node.inputs[0].default_value = 0.5
-        node.operation = "LOGARITHM"
         node.inputs[1].default_value = 51.4
-        node.operation = "LOGARITHM"
         node.inputs[2].default_value = 0.0
-        node.operation = "LOGARITHM"
         node.outputs[0].default_value = 0.0
 
         node = nodes.new("GeometryNodeAttributeMapRange")
         node.name = "Attribute Map Range"
         node.location = (-547, 371)
         node.data_type = "FLOAT"
+        node.interpolation_type = "LINEAR"
         node.inputs[1].default_value = "foam"
-        node.data_type = "FLOAT"
         node.inputs[2].default_value = "foamLim"
-        node.data_type = "FLOAT"
         node.inputs[3].default_value = 0.25
-        node.data_type = "FLOAT"
         node.inputs[4].default_value = 1.0
-        node.data_type = "FLOAT"
         node.inputs[5].default_value = 0.0
-        node.data_type = "FLOAT"
         node.inputs[6].default_value = 1.0
-        node.data_type = "FLOAT"
         node.inputs[7].default_value = 4.0
-        node.data_type = "FLOAT"
         node.inputs[8].default_value = (0.0, 0.0, 0.0,)
-        node.data_type = "FLOAT"
         node.inputs[9].default_value = (1.0, 1.0, 1.0,)
-        node.data_type = "FLOAT"
         node.inputs[10].default_value = (0.0, 0.0, 0.0,)
-        node.data_type = "FLOAT"
         node.inputs[11].default_value = (1.0, 1.0, 1.0,)
-        node.data_type = "FLOAT"
         node.inputs[12].default_value = (4.0, 4.0, 4.0,)
-        node.data_type = "FLOAT"
         node.inputs[13].default_value = False
-        node.data_type = "FLOAT"
 
         node = nodes.new("GeometryNodeAttributeMix")
         node.name = "Attribute Mix"
         node.location = (-286, 357)
         node.input_type_a = "ATTRIBUTE"
+        node.input_type_b = "ATTRIBUTE"
+        node.input_type_factor = "FLOAT"
+        node.blend_type = "ADD"
         node.inputs[1].default_value = ""
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[2].default_value = 0.5
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[3].default_value = "foamLim"
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[4].default_value = 0.0
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[5].default_value = (0.0, 0.0, 0.0,)
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[6].default_value = (0.5, 0.5, 0.5, 1.0,)
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[7].default_value = "dp_wetmap"
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[8].default_value = 0.0
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[9].default_value = (0.0, 0.0, 0.0,)
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[10].default_value = (0.5, 0.5, 0.5, 1.0,)
-        node.input_type_a = "ATTRIBUTE"
         node.inputs[11].default_value = "foamLim"
-        node.input_type_a = "ATTRIBUTE"
 
         node = nodes.new("NodeReroute")
         node.name = "Reroute.005"
@@ -200,120 +180,107 @@ class AOMGeoNodesHandler:
 
         node = nodes.new("NodeReroute")
         node.name = "Reroute.006"
-        node.location = (4, 97)
+        node.location = (-4, 139)
         node.inputs[0].default_value = 0.0
         node.outputs[0].default_value = 0.0
 
         node = nodes.new("GeometryNodePointDistribute")
         node.name = "Point Distribute"
-        node.location = (76, 264)
+        node.location = (64, 348)
         node.distribute_method = "RANDOM"
         node.inputs[1].default_value = 0.0
-        node.distribute_method = "RANDOM"
         node.inputs[2].default_value = 30.0
-        node.distribute_method = "RANDOM"
         node.inputs[3].default_value = "foamLim"
-        node.distribute_method = "RANDOM"
         node.inputs[4].default_value = 0
-        node.distribute_method = "RANDOM"
+
+        node = nodes.new("GeometryNodeAttributeVectorMath")
+        node.name = "Attribute Vector Math.001"
+        node.location = (289, 331)
+        node.operation = "SCALE"
+        node.input_type_a = "ATTRIBUTE"
+        node.input_type_b = "FLOAT"
+        node.inputs[1].default_value = "spray"
+        node.inputs[2].default_value = (0.0, 0.0, 0.0,)
+        node.inputs[3].default_value = ""
+        node.inputs[4].default_value = (0.0, 0.0, 0.0,)
+        node.inputs[5].default_value = 0.0
+        node.inputs[6].default_value = ""
+        node.inputs[7].default_value = (0.0, 0.0, 0.0,)
+        node.inputs[8].default_value = 0.0
+        node.inputs[9].default_value = "spray"
 
         node = nodes.new("GeometryNodeAttributeVectorMath")
         node.name = "Attribute Vector Math"
-        node.location = (297, 307)
+        node.location = (520, 341)
         node.operation = "ADD"
+        node.input_type_a = "ATTRIBUTE"
+        node.input_type_b = "ATTRIBUTE"
         node.inputs[1].default_value = "position"
-        node.operation = "ADD"
         node.inputs[2].default_value = (0.0, 0.0, 0.0,)
-        node.operation = "ADD"
         node.inputs[3].default_value = "spray"
-        node.operation = "ADD"
         node.inputs[4].default_value = (0.0, 0.0, 0.0,)
-        node.operation = "ADD"
         node.inputs[5].default_value = 0.0
-        node.operation = "ADD"
         node.inputs[6].default_value = ""
-        node.operation = "ADD"
         node.inputs[7].default_value = (0.0, 0.0, 0.0,)
-        node.operation = "ADD"
         node.inputs[8].default_value = 0.0
-        node.operation = "ADD"
         node.inputs[9].default_value = "position"
-        node.operation = "ADD"
 
         node = nodes.new("GeometryNodeAttributeRandomize")
         node.name = "Attribute Randomize"
-        node.location = (502, 272)
+        node.location = (673, 87)
         node.data_type = "FLOAT"
+        node.operation = "REPLACE_CREATE"
         node.inputs[1].default_value = "scale"
-        node.data_type = "FLOAT"
         node.inputs[2].default_value = (0.0, 0.0, 0.0,)
-        node.data_type = "FLOAT"
         node.inputs[3].default_value = (1.0, 1.0, 1.0,)
-        node.data_type = "FLOAT"
         node.inputs[4].default_value = 0.01
-        node.data_type = "FLOAT"
         node.inputs[5].default_value = 0.8
-        node.data_type = "FLOAT"
         node.inputs[6].default_value = 0
-        node.data_type = "FLOAT"
         node.inputs[7].default_value = 100
-        node.data_type = "FLOAT"
         node.inputs[8].default_value = 0
-        node.data_type = "FLOAT"
 
         node = nodes.new("GeometryNodePointScale")
         node.name = "Point Scale"
-        node.location = (726, 230)
+        node.location = (893, 230)
         node.input_type = "FLOAT"
         node.inputs[1].default_value = "scale"
-        node.input_type = "FLOAT"
         node.inputs[2].default_value = (
             0.029999999329447746, 0.029999999329447746, 0.029999999329447746,)
-        node.input_type = "FLOAT"
         node.inputs[3].default_value = 1.0
-        node.input_type = "FLOAT"
 
         node = nodes.new("GeometryNodePointInstance")
         node.name = "Point Instance"
-        node.location = (923, 179)
+        node.location = (1090, 179)
         node.instance_type = "COLLECTION"
+        node.use_whole_collection = False
         node.inputs[1].default_value = None
-        node.instance_type = "COLLECTION"
         node.inputs[2].default_value = None
-        node.instance_type = "COLLECTION"
         node.inputs[3].default_value = 0
-        node.instance_type = "COLLECTION"
 
         node = nodes.new("GeometryNodeJoinGeometry")
         node.name = "Join Geometry"
-        node.location = (1139, 173)
+        node.location = (1306, 173)
 
         node = nodes.new("NodeGroupOutput")
         node.name = "Group Output"
-        node.location = (1324, 176)
+        node.location = (1491, 176)
         links.new(nodes['Group Input'].outputs[0],
                   nodes['Join Geometry'].inputs[0])
         links.new(nodes['Group Input'].outputs[0],
                   nodes['Attribute Map Range'].inputs[0])
-        links.new(nodes['Group Input'].outputs[1],
+
+        links.new(nodes['Group Input'].outputs[2],
                   nodes['Reroute.005'].inputs[0])
-        links.new(nodes['Group Input'].outputs[2],  nodes['Math'].inputs[0])
-        links.new(nodes['Group Input'].outputs[3],
-                  nodes['Attribute Randomize'].inputs[3])
-        links.new(nodes['Group Input'].outputs[4],
-                  nodes['Attribute Randomize'].inputs[2])
-        links.new(nodes['Group Input'].outputs[5],
-                  nodes['Point Scale'].inputs[1])
-        links.new(nodes['Group Input'].outputs[6],
-                  nodes['Attribute Mix'].inputs[1])
-        links.new(nodes['Group Input'].outputs[7],
+        links.new(nodes['Group Input'].outputs[3],  nodes['Math'].inputs[0])
+
+        links.new(nodes['Group Input'].outputs[8],
                   nodes['Point Instance'].inputs[2])
         links.new(nodes['Point Instance'].outputs['Geometry'],
                   nodes['Join Geometry'].inputs['Geometry'])
         links.new(nodes['Attribute Vector Math'].outputs['Geometry'],
                   nodes['Attribute Randomize'].inputs['Geometry'])
         links.new(nodes['Point Distribute'].outputs['Geometry'],
-                  nodes['Attribute Vector Math'].inputs['Geometry'])
+                  nodes['Attribute Vector Math.001'].inputs['Geometry'])
         links.new(nodes['Join Geometry'].outputs['Geometry'],
                   nodes['Group Output'].inputs['Geometry'])
         links.new(nodes['Point Scale'].outputs['Geometry'],
@@ -332,12 +299,28 @@ class AOMGeoNodesHandler:
                   nodes['Attribute Map Range'].inputs['From Min'])
         links.new(nodes['Attribute Mix'].outputs['Geometry'],
                   nodes['Point Distribute'].inputs['Geometry'])
-        mod['Input_5'] = 17.0
-        mod['Input_17'] = 1.17
-        mod['Input_7'] = 1.56
-        mod['Input_9'] = 0.17
-        mod['Input_13'] = 0.06
-        mod['Input_19'] = 0.5
+        links.new(nodes['Attribute Vector Math.001'].outputs['Geometry'],
+                  nodes['Attribute Vector Math'].inputs['Geometry'])
+
+        #############################################
+
+        links.new(nodes['Group Input'].outputs[4],
+                  nodes['Attribute Randomize'].inputs[5])
+        links.new(nodes['Group Input'].outputs[5],
+                  nodes['Attribute Randomize'].inputs[4])
+        links.new(nodes['Group Input'].outputs[6],
+                  nodes['Point Scale'].inputs[3])
+        links.new(nodes['Group Input'].outputs[7],
+                  nodes['Attribute Mix'].inputs[2])
+        links.new(nodes['Group Input'].outputs[1],
+                  nodes['Attribute Vector Math.001'].inputs[5])
+
+        node_group.inputs[7].min_value = 0
+        node_group.inputs[7].max_value = 1
+
+        # for inp in node_group.inputs:
+        #    if inp.bl_socket_idname != 'NodeSocketGeometry':
+        #        mod[inp.identifier] = inp.default_value
 
     def remove_spray(self, context, ocean):
         if "Spray" in ocean.modifiers:
--- nodes_to_code.py
+++ nodes_to_code.py
@@ -4,6 +4,8 @@
 # [] test material node trees
 # [] clean up get value section to bl_idname
 
+# set last values mod[input macht probleme]
+
 
 import bpy
 
@@ -100,11 +102,11 @@ def nodes_to_nodecode(group):
             pass
 
         else:
+            special_operations(node)
             for a, inp in enumerate(node.inputs):
                 value = get_putput(a, inp)
                 if value != '':
                     print(f'node.inputs[{a}].default_value = {value}')
-                special_operations(node)
 
             for a, inp in enumerate(node.outputs):
                 value = get_putput(a, inp)
@@ -120,27 +122,28 @@ def special_operations(node):
 
     if hasattr(node, "instance_type"):
         print(f'node.instance_type = "{node.instance_type}"')
-    elif hasattr(node, "input_type"):
+    if hasattr(node, "input_type"):
         print(f'node.input_type = "{node.input_type}"')
-    elif hasattr(node, "data_type"):
+    if hasattr(node, "data_type"):
         print(f'node.data_type = "{node.data_type}"')
-    elif hasattr(node, "operation"):
+    if hasattr(node, "operation"):
         print(f'node.operation = "{node.operation}"')
-    elif hasattr(node, "input_type_a"):
+    if hasattr(node, "input_type_a"):
         print(f'node.input_type_a = "{node.input_type_a}"')
-    elif hasattr(node, "input_type_b"):
+    if hasattr(node, "input_type_b"):
         print(f'node.input_type_b = "{node.input_type_b}"')
-
-    elif hasattr(node, "distribute_method"):
+    if hasattr(node, "distribute_method"):
         print(f'node.distribute_method = "{node.distribute_method}"')
-    elif hasattr(node, "input_type_factor"):
+    if hasattr(node, "input_type_factor"):
         print(f'node.input_type_factor = "{node.input_type_factor}"')
-    elif hasattr(node, "blend_type"):
-        print(f'node.blend_type = {node.blend_type}')
-    elif hasattr(node, "interpolation_type"):
+    if hasattr(node, "blend_type"):
+        print(f'node.blend_type = "{node.blend_type}"')
+    if hasattr(node, "interpolation_type"):
         print(f'node.interpolation_type = "{node.interpolation_type}"')
-    elif hasattr(node, "use_clamp"):
-        print(f'node.use_clamp = "{node.use_clamp}"')
+    if hasattr(node, "use_clamp"):
+        print(f'node.use_clamp = {node.use_clamp}')
+    if hasattr(node, "use_whole_collection"):
+        print(f'node.use_whole_collection = {node.use_whole_collection}')
 
 
 def get_sorted_InputLinks(nodes, links):
@@ -183,20 +186,35 @@ def get_putput_index(node, socket, inout):
                 break
     return index
 
+# used to set the input socket idname to the target of the link
+
+
+def get_idname_from_targetsocket(group, inp):
+    #input_node = get_inputnode(group.nodes)
+    targetidname = 'MOOOP'
+    for link in group.links:
+        # ärint(link.from_socket)
+        # print(link.to_socket)
+        if link.from_socket.name == inp.name:
+            targetidname = link.to_socket.bl_idname
+
+    return targetidname
+
 
 def gen_maininputs(group):
     for inp in group.inputs:
         if inp.bl_socket_idname != 'NodeSocketGeometry':
             print(
-                f"inp = node_group.inputs.new('{inp.bl_socket_idname}','{inp.name}')")
+                f"inp = node_group.inputs.new('{get_idname_from_targetsocket(group, inp)}','{inp.name}')")
             if hasattr(inp, "default_value"):
                 if get_value(inp.default_value) != '':
                     print(
                         f"inp.default_value = {get_value(inp.default_value)}")
-            elif hasattr(inp, "min_value"):
+            # max min causing problems: controller will be set to min or max (inf, ) not the default
+            '''if hasattr(inp, "min_value"):
                 print(f"inp.min_value = {inp.max_value}")
-            elif hasattr(inp, "max_value"):
-                print(f"inp.max_value = {inp.max_value}")
+            if hasattr(inp, "max_value"):
+                print(f"inp.max_value = {inp.max_value}")'''
 
     # group.inputs
 
